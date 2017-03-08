df1e8c27cf9031b328bd98172bfe1a944659b9e1
Frank Pommerening <frank@pommerening-online.de>
2017-03-08 22:25:36 +0100

    Add Sample buildoutside

---
+++ Samples/BuildOutside/src/WebApp/Modules/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FP.Spartakiade2017.Docker.BuildOutside.WebApp.Modules
+{
+    public class HomeModule : Nancy.NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => $"Hallo Spartakiade - {DateTime.Now}");
+        }
+    }
+}

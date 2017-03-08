5a245fe6b2aa9625cdb243b4f29c08a4d0893666
Frank Pommerening <frank@pommerening-online.de>
2017-03-08 20:47:47 +0100

    Add Sample Buildinside

---
+++ Samples/BuildInside/WebApp/Module/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FP.Spartakiade2017.Docker.BuildInside.WebApp.Module
+{
+    public class HomeModule : Nancy.NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => $"Hallo Spartakiade - {DateTime.Now}");
+        }
+    }
+}
---
+++ Samples/BuildInside/WebApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace FP.Spartakiade2017.Docker.BuildInside.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .UseKestrel()
+               .UseStartup<Startup>()
+               .UseUrls("http://0.0.0.0:5000")
+               .Build();
+
+            host.Run();
+        }
+    }
+}

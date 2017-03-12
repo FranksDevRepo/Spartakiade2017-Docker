6274445a0173e72d38656bcb5befc9b45246165a
Frank Pommerening <frank.pommerening@axp-consulting.de>
2017-03-12 10:14:35 +0100

    webhock: bootstrap

---
+++ Samples/Webhock/src/Service/Modules/DockerHubModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Nancy;
+
+namespace FP.Spartakiade2017.Docker.WebHock.Service.Modules
+{
+    public class DockerHubModule : NancyModule
+    {
+        public DockerHubModule()
+        {
+            Post("/dockerhub/", async args =>
+            {
+                using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+                {
+                    var contentAsText = await reader.ReadToEndAsync();
+                    Console.WriteLine(contentAsText);
+                }
+                return HttpStatusCode.OK;
+            });
+        }
+    }
+}
